1494eee37676f2d7d6fb5e0cbd314effb73ede0c
Matt <matt.sonnhalter@gmail.com>
2017-02-16 01:58:29 -0600

    Add movie class

---
+++ src/MovieCollection.Web/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieCollection.Web.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Tagline { get; set; }
+        
+        public string ImageUrl { get; set; } 
+    }
+}
