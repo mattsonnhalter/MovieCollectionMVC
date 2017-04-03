d105ba58dc8c52bf834a8f0c0817a26cf5b90241
Matt <matt.sonnhalter@gmail.com>
2017-04-03 01:30:32 -0500

    Add placeholder for css classes and DisplayName annotation

--- src/MovieCollection.Web/Models/Search.cs
+++ src/MovieCollection.Web/Models/Search.cs
@@ -1,7 +1,10 @@
-﻿namespace MovieCollection.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCollection.Web.Models
 {
     public class Search
     {
+        [Display(Name = "Search Title")]
         public string Title { get; set; }
     }
 }
