10c925f47d6aca143d34a24611063ea16b57171c
Matt <matt.sonnhalter@gmail.com>
2017-05-06 12:05:19 -0500

    Moved Movie searching into main page

--- src/MovieCollection.Web/Controllers/HomeController.cs
+++ src/MovieCollection.Web/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MovieCollection.Web.Models;
+using MovieCollection.Web.Viewmodels;
 using TMDbLib.Client;
-using Movie = MovieCollection.Web.Models.Movie;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
 
 namespace MovieCollection.Web.Controllers
 {
@@ -9,7 +13,30 @@ namespace MovieCollection.Web.Controllers
         [HttpGet]
         public ActionResult Index()
         {       
-            return View();
+            return View(new MovieIndexViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(MovieIndexViewModel movieViewModel)
+        {
+            movieViewModel.movieModels = new List<Movie>();
+
+            TMDbClient client = new TMDbClient("1d51304d2d0506fca98f49b582707408");
+            SearchContainer<SearchMovie> movieApiResults = client.SearchMovieAsync(movieViewModel.searchModel.Title).Result;
+
+            foreach (var m in movieApiResults.Results)
+            {
+                Movie localMovie = new Movie();
+
+                localMovie.Id = m.Id;
+                localMovie.Title = m.Title;
+                localMovie.ReleaseDate = m.ReleaseDate;
+                localMovie.ImageUrl = "http://image.tmdb.org/t/p/w185/" + m.PosterPath;
+
+                movieViewModel.movieModels.Add(localMovie);
+            }
+
+            return View(movieViewModel);
         }
     }
 }
